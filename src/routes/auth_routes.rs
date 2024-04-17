6b5281be38e2785d918cd8aa6c8d80f35fd98ffa
Diego <dgox16@gmail.com>
2024-04-17 09:19:19 -0600

    Se agrego la funcionalidad del inicio de sesion

--- src/routes/auth_routes.rs
+++ src/routes/auth_routes.rs
@@ -2,10 +2,14 @@ use std::sync::Arc;
 
 use axum::{routing::post, Router};
 
-use crate::{handlers::auth_handlers::register_user_handler, AppState};
+use crate::{
+    handlers::auth_handlers::{login_user_handler, register_user_handler},
+    AppState,
+};
 
 pub fn auth_router(app_state: Arc<AppState>) -> Router {
     Router::new()
         .route("/api/auth/register", post(register_user_handler))
+        .route("/api/auth/login", post(login_user_handler))
         .with_state(app_state)
 }
