3f41552c980e1bf7d39cd97b87e96d41f5b2b84d
Diego <dgox16@gmail.com>
2024-04-19 10:32:32 -0600

    Se agrego el modelo de polizas

---
+++ migrations/20240419142752_stage7.down.sql
@@ -0,0 +1,8 @@
+-- Add down migration script here
+DROP TABLE polizas;
+
+DROP TYPE tipo_poliza;
+
+DROP TYPE aplicacion_poliza;
+
+DROP TYPE fuente_poliza;
---
+++ migrations/20240419142752_stage7.up.sql
@@ -0,0 +1,35 @@
+CREATE TYPE tipo_poliza AS ENUM ('egreso', 'ingreso', 'diario');
+
+CREATE TYPE aplicacion_poliza AS ENUM (
+    'normal',
+    'condonacion',
+    'cheque_orden',
+    'cierre_diario',
+    'cierre_mensual',
+    'cierre_anual'
+);
+
+CREATE TYPE fuente_poliza AS ENUM (
+    'operacion',
+    'activos',
+    'nomina',
+    'gastos',
+    'pasiva',
+    'traslados',
+    'traspasos'
+);
+
+CREATE TABLE polizas (
+    id_poliza SERIAL PRIMARY KEY,
+    tipo tipo_poliza NOT NULL,
+    numero INTEGER DEFAULT 1,
+    sucursal INTEGER NOT NULL,
+    fecha_poliza TIMESTAMP WITH TIME ZONE DEFAULT NOW (),
+    fecha_registro_poliza TIMESTAMP WITH TIME ZONE DEFAULT NOW (),
+    concepto VARCHAR(150) NOT NULL,
+    usuario_autoriza INTEGER DEFAULT 1,
+    usuario_elabora INTEGER NOT NULL,
+    aplicacion aplicacion_poliza DEFAULT 'normal',
+    fuente fuente_poliza DEFAULT 'operacion',
+    automatico BOOLEAN DEFAULT FALSE
+)
