b5fa1b4024c333867259d6809845933beeace429
Diego <dgox16@gmail.com>
2024-04-25 10:45:06 -0600

    Se agrego la tabla de cuentas en la base de datos

---
+++ migrations/20240425155111_stage10.down.sql
@@ -0,0 +1,9 @@
+DROP TABLE cuentas;
+
+DROP TYPE clasificacion_cuenta;
+
+DROP TYPE finalidad_cuenta;
+
+DROP TYPE naturaleza_cuenta;
+
+DROP TYPE grupo_cuenta;
---
+++ migrations/20240425155111_stage10.up.sql
@@ -0,0 +1,38 @@
+CREATE TYPE clasificacion_cuenta AS ENUM ('capitulo', 'subcapitulo', 'mayor', 'menor');
+
+CREATE TYPE grupo_cuenta AS ENUM (
+    'activo',
+    'pasivo',
+    'capital',
+    'resultado_acreedor',
+    'resultado_deudor',
+    'orden',
+    'puente'
+);
+
+CREATE TYPE finalidad_cuenta AS ENUM ('caja', 'banco', 'otros');
+
+CREATE TYPE naturaleza_cuenta AS ENUM ('deudora', 'acreedora');
+
+CREATE TABLE cuentas (
+    id_cuenta SERIAL PRIMARY KEY,
+    cuenta VARCHAR(30) NOT NULL,
+    cuenta_siti VARCHAR(15) NOT NULL,
+    nombre VARCHAR(60) NOT NULL,
+    clasificacion clasificacion_cuenta DEFAULT 'capitulo',
+    grupo grupo_cuenta DEFAULT 'activo',
+    finalidad finalidad_cuenta DEFAULT 'otros',
+    naturaleza naturaleza_cuenta DEFAULT 'deudora',
+    afectable BOOLEAN DEFAULT FALSE,
+    padre VARCHAR(30) NOT NULL,
+    nivel INTEGER NOT NULL,
+    en_balance BOOLEAN DEFAULT FALSE,
+    en_catalogo_minimo BOOLEAN NOT NULL,
+    nombre_balance VARCHAR(100) NOT NULL,
+    nombre_siti VARCHAR(70) NOT NULL,
+    cuenta_padre_siti VARCHAR(20) NOT NULL,
+    cuenta_agrupar VARCHAR(30) NOT NULL,
+    orden_siti INTEGER NOT NULL,
+    subcuenta_siti BOOLEAN DEFAULT FALSE,
+    prorrateo BOOLEAN DEFAULT FALSE
+);
