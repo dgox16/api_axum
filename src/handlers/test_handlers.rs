52926759f6b195a034be6853c69e23b004050dab
Diego <dgox16@gmail.com>
2024-05-06 19:04:24 -0600

    Se tradujo el healthcheck

--- src/handlers/test_handlers.rs
+++ src/handlers/test_handlers.rs
@@ -2,11 +2,11 @@ use axum::{response::IntoResponse, Json};
 
 // Funcion solo para comprobar que el servidor sirve
 pub async fn health_checker_handler() -> impl IntoResponse {
-    const MESSAGE: &str = "The API works perfectly";
+    const MESSAGE: &str = "La API funciona correctamente";
 
     let json_response = serde_json::json!({
-        "status": "success",
-        "message": MESSAGE
+        "estado": true,
+        "mensaje": MESSAGE
     });
 
     Json(json_response)
