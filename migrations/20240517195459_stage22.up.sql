6610674e7f6c6fb6295923e1d6cb3484b6b3eeed
Diego <dgox16@gmail.com>
2024-05-17 14:59:55 -0600

    Se lleno la tabla de escolaridades

--- migrations/20240517195459_stage22.up.sql
+++ migrations/20240517195459_stage22.up.sql
@@ -1,5 +1,16 @@
 CREATE TABLE escolaridades (
     id_escolaridad SERIAL PRIMARY KEY,
-    nombre VARCHAR(60) NOT NULL,
+    nombre VARCHAR(60) NOT NULL
 );
+
+INSERT INTO escolaridades (id_escolaridad, nombre) VALUES
+(1,	'MATERNAL'),
+(2,	'PREESCOLAR'),
+(3,	'PRIMARIA'),
+(4,	'SECUNDARIA'),
+(5,	'PREPARATORIA'),
+(6,	'LICENCIATURA'),
+(7,	'MAESTRIA'),
+(8,	'DOCTORADO'),
+(9,	'SIN ESTUDIOS');
 
