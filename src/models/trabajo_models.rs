237b262ced36e7cf97ee9b447ee98e5487e4b53e
Diego <dgox16@gmail.com>
2024-05-17 17:38:19 -0600

    Se agrego el modelo de profesiones

--- src/models/trabajo_models.rs
+++ src/models/trabajo_models.rs
@@ -6,3 +6,11 @@ pub struct OcupacionModelo {
     pub nombre: String,
     pub ponderacion_5c: f32,
 }
+
+#[derive(Debug, sqlx::FromRow, Deserialize, Serialize)]
+pub struct ProfesionModelo {
+    pub id_profesion: i32,
+    pub profesion_migrado: i64,
+    pub nombre: String,
+    pub abreviatura: String,
+}
