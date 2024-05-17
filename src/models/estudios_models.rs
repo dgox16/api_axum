66c405c144eabb7d97df2e9d0d740f593161c08a
Diego <dgox16@gmail.com>
2024-05-17 17:01:24 -0600

    Se creo el modelo de lenguas

--- src/models/estudios_models.rs
+++ src/models/estudios_models.rs
@@ -5,3 +5,18 @@ pub struct EscolaridadModelo {
     pub id_escolaridad: i32,
     pub nombre: String,
 }
+
+#[derive(Clone, Debug, PartialEq, PartialOrd, sqlx::Type, Deserialize, Serialize)]
+#[sqlx(type_name = "tipo_lengua", rename_all = "lowercase")]
+pub enum TipoLengua {
+    Oficial,
+    Indigena,
+    Extranjera,
+}
+
+#[derive(Debug, sqlx::FromRow, Deserialize, Serialize)]
+pub struct LenguasModelo {
+    pub id_lengua: i32,
+    pub nombre: String,
+    pub tipo: TipoLengua,
+}
