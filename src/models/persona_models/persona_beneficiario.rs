793d1528bdb5b4bfd3ba08a5ba4a271609370225
Diego <dgox16@gmail.com>
2024-05-20 13:10:40 -0600

    Se arreglo un error de nombre

--- src/models/persona_models/persona_beneficiario.rs
+++ src/models/persona_models/persona_beneficiario.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, sqlx::FromRow, Deserialize, Serialize)]
-pub struct TutorPersonaModelo {
+pub struct BeneficiarioPersonaModelo {
     pub id_persona_beneficiario: i32,
     pub id_persona: i32,
     pub entre_calle: i32,
