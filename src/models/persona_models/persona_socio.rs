f2f231cb00281e8ec2176d5446016c078e12f69f
Diego <dgox16@gmail.com>
2024-05-20 10:03:51 -0600

    Se creo el modelo para los socios

--- src/models/persona_models/persona_socio.rs
+++ src/models/persona_models/persona_socio.rs
@@ -1,7 +1,67 @@
+use chrono::NaiveDate;
 use serde::{Deserialize, Serialize};
+
+use super::persona_principal::{ClasificacionPersona, PeriodoPersona};
 
 #[derive(Debug, sqlx::FromRow, Deserialize, Serialize)]
 pub struct ManerasEnterarse {
     id_manera_enterarse: i32,
     nombre: String,
+}
+
+#[derive(Clone, Debug, PartialEq, PartialOrd, sqlx::Type, Deserialize, Serialize)]
+#[sqlx(type_name = "propietario_persona", rename_all = "lowercase")]
+pub enum EsPropietarioPersona {
+    Si,
+    No,
+    Autoempleo,
+}
+
+#[derive(Clone, Debug, PartialEq, PartialOrd, sqlx::Type, Deserialize, Serialize)]
+#[sqlx(type_name = "regimen_conyugal_persona", rename_all = "lowercase")]
+pub enum RegimenConyugalPersona {
+    NoAplica,
+    BienesMancomunados,
+    BienesSeparados,
+}
+
+#[derive(Debug, sqlx::FromRow, Deserialize, Serialize)]
+pub struct SocioPersonaModelo {
+    pub id_persona_socio: i32,
+    pub id_persona: i32,
+    pub clasificacion: ClasificacionPersona,
+    pub ocupacion_pld: i32,
+    pub especificacion_pld: i32,
+    pub actividad_pld: i32,
+    pub antiguedad: i32,
+    pub periodo: PeriodoPersona,
+    pub frecuencia_captacion: i32,
+    pub operacion_maxima_captacion: f32,
+    pub perfil_frecuencia_prestamo: i32,
+    pub operacion_maxima_prestamo: f32,
+    pub ingresos_mensual: f32,
+    pub egresos_mensual: f32,
+    pub grado_afectacion: i32,
+    pub afectacion: f32,
+    pub proveedor_recursos: i32,
+    pub parentesco: i32,
+    pub persona_recomendo: i32,
+    pub manera_enterarse: i32,
+    pub lengua: i32,
+    pub empresa: i32,
+    pub puesto: i32,
+    pub fecha_trabajo: NaiveDate,
+    pub ingreso_ordinario: f32,
+    pub otros_ingresos: f32,
+    pub es_propietario: EsPropietarioPersona,
+    pub entre_calle: i32,
+    pub y_calle: i32,
+    pub fecha_residencia: NaiveDate,
+    pub lugar_nacimiento: String,
+    pub estado_nacimiento: i32,
+    pub regimen_conyugal: RegimenConyugalPersona,
+    pub profesion: i32,
+    pub escolaridad: i32,
+    pub autorizo_compartir_informacion_ifai: bool,
+    pub autorizo_publicidad: bool,
 }
