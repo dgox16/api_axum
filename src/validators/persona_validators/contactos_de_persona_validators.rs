0056c68a66a5f7cd0eb5e897670eb3e355983b73
Diego <dgox16@gmail.com>
2024-05-28 09:54:24 -0600

    Se mejoro la validacion en los nuevos contactos

--- src/validators/persona_validators/contactos_de_persona_validators.rs
+++ src/validators/persona_validators/contactos_de_persona_validators.rs
@@ -1,17 +1,42 @@
 use axum::{http::StatusCode, Json};
 
-use crate::schemas::persona_schemas::contactos_de_persona_schemas::CrearContactoDePersonaSchema;
+use crate::{
+    models::persona_models::contactos_de_persona_models::TipoContacto,
+    schemas::persona_schemas::contactos_de_persona_schemas::CrearContactoDePersonaSchema,
+};
 
 pub fn validar_nuevo_contacto_de_persona(
     contactos_de_persona: &Vec<CrearContactoDePersonaSchema>,
 ) -> Result<(), (StatusCode, Json<serde_json::Value>)> {
     for contacto in contactos_de_persona {
-        if contacto.contacto.trim().is_empty() {
-            let respuesta_error = serde_json::json!({
-                "estado": "error",
-                "mensaje": "El contacto no puede estar vacio",
-            });
-            return Err((StatusCode::BAD_REQUEST, Json(respuesta_error)));
+        match contacto.tipo {
+            TipoContacto::TelefonoFijo => {
+                if contacto.contacto.trim().is_empty() || contacto.contacto.trim().len() != 10 {
+                    let respuesta_error = serde_json::json!({
+                        "estado": "error",
+                        "mensaje": "El formato del telefono es incorrecto",
+                    });
+                    return Err((StatusCode::BAD_REQUEST, Json(respuesta_error)));
+                }
+            }
+            TipoContacto::TelefonoMovil => {
+                if contacto.contacto.trim().is_empty() || contacto.contacto.trim().len() != 10 {
+                    let respuesta_error = serde_json::json!({
+                        "estado": "error",
+                        "mensaje": "El formato del telefono es incorrecto",
+                    });
+                    return Err((StatusCode::BAD_REQUEST, Json(respuesta_error)));
+                }
+            }
+            _ => {
+                if contacto.contacto.trim().is_empty() {
+                    let respuesta_error = serde_json::json!({
+                        "estado": "error",
+                        "mensaje": "El contacto no puede estar vacio",
+                    });
+                    return Err((StatusCode::BAD_REQUEST, Json(respuesta_error)));
+                }
+            }
         }
     }
 
